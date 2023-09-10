8419050650362209427c09767799b1fe9c191470
Veronika Čamborová <121693001+VeveCambor@users.noreply.github.com>
2023-09-10 11:13:08 +0000

    change custom select

--- effects.js
+++ effects.js
@@ -10,3 +10,32 @@ document.addEventListener('click', e => {
         cursor.classList.remove("expand");
     }, 500);
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    selectItems.style.display = 'none';
+  });
+  
+  const customSelect = document.querySelector('.custom-select');
+  const selectSelected = customSelect.querySelector('.select-selected');
+  const selectItems = customSelect.querySelector('.select-items');
+  const selectItemElements = customSelect.querySelectorAll('.select-items li');
+  const selectedCityInput = document.getElementById('selected-city');
+  
+  selectSelected.addEventListener('click', function (e) {
+    e.stopPropagation(); 
+    selectItems.style.display = selectItems.style.display === 'block' ? 'none' : 'block';
+  });
+  
+  selectItemElements.forEach(function (item) {
+    item.addEventListener('click', function () {
+      selectSelected.value = item.textContent;
+      selectedCityInput.value = item.getAttribute('data-value');
+      selectItems.style.display = 'none';
+    });
+  });
+  
+  window.addEventListener('click', function () {
+    selectItems.style.display = 'none';
+  });
+  
